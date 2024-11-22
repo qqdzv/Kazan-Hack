0fc7dccceabf148ece4511a1d3fcf6163a6a21ac
pepe <m2308244@edu.misis.ru>
2024-11-22 11:33:08 +0300

    test2

--- backend/src/z.py
+++ backend/src/z.py
@@ -1,32 +1,34 @@
 import asyncio
 import websockets
 import json
+from src.logger import logger
+
 
 clients = set()
 
 async def start_websocket_server():
-    print("Z")
+    logger.info("Z")
     async def handle_connection(websocket, path):
         # Подключение нового клиента
-        print("ZZZZZ")
+        logger.info("ZZZZZ")
         clients.add(websocket)
         try:
             async for message in websocket:
                 # Получаем сообщение от клиента
-                print(f"Received message: {message}")
+                logger.info(f"Received message: {message}")
 
                 # Рассылаем это сообщение всем остальным клиентам
                 for client in clients:
                     if client != websocket:
                         await client.send(message)
         except websockets.exceptions.ConnectionClosed:
-            print("Connection closed")
+            logger.info("Connection closed")
         finally:
             # Убираем клиента из списка при закрытии соединения
             clients.remove(websocket)
 
     # Запуск WebSocket сервера на порту 5000
     server = await websockets.serve(handle_connection, "skin-cancer.ru", 5000)
-    print("WebSocket server started at ws://localhost:5000")
+    logger.info("WebSocket server started at ws://localhost:5000")
     await server.wait_closed()
     
