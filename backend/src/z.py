fb2a3e24301c0b53b2832e560e367da244ef3b16
pepe <m2308244@edu.misis.ru>
2024-11-22 11:40:15 +0300

    KDK

--- backend/src/z.py
+++ backend/src/z.py
@@ -9,26 +9,26 @@ clients = set()
 async def start_websocket_server():
     logger.info("Z")
     async def handle_connection(websocket, path):
-        # Подключение нового клиента
-        logger.info("ZZZZZ")
+        logger.info(f"New connection: {path}")
         clients.add(websocket)
         try:
             async for message in websocket:
-                # Получаем сообщение от клиента
                 logger.info(f"Received message: {message}")
-
-                # Рассылаем это сообщение всем остальным клиентам
                 for client in clients:
                     if client != websocket:
-                        await client.send(message)
-        except websockets.exceptions.ConnectionClosed:
-            logger.info("Connection closed")
+                        try:
+                            await client.send(message)
+                        except Exception as e:
+                            logger.error(f"Error sending message: {e}")
+        except Exception as e:
+            logger.error(f"Connection error: {e}")
         finally:
-            # Убираем клиента из списка при закрытии соединения
             clients.remove(websocket)
+            logger.info("Client disconnected")
+
 
     # Запуск WebSocket сервера на порту 5000
-    server = await websockets.serve(handle_connection, "skin-cancer.ru", 5000)
+    server = await websockets.serve(handle_connection, "0.0.0.0", 5000)
     logger.info("WebSocket server started at ws://localhost:5000")
     await server.wait_closed()
     
